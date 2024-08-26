4b8dc0154f1cf91b32c96fec44bd576f90ade219
Glen357 <161297213+Glen357@users.noreply.github.com>
2024-08-26 22:15:43 +1200

    tried to retrive data from DB, no luck. Commiting before shutdown

--- Server.js
+++ Server.js
@@ -29,29 +29,35 @@ db.getConnection((err, connection) => {
     return;
   }
   console.log("Connected to the database");
-  connection.release(); // Release the connection back to the pool
+  // connection.release(); // Release the connection back to the pool
 });
 
 // Define a simple route to test the server
-app.get("/api/selector_table", (req, res) => {
-  res.send("Server is live and connected to the database");
+app.get("/api/claims-calculator/selector_table", (req, res) => {
+  res.json({ message: "Server is live and connected to the database" });
 });
 
 // Start the server
-const PORT = process.env.PORT || 3000;
+const PORT = process.env.PORT;
 app.listen(PORT, () => {
   console.log(`Server is live at http://localhost:${PORT}`);
 });
 
-// app.get("/api/selector_table", (req, res) => {
-//   const sqlQuery = "SELECT * FROM `claims-calculator`.selector_table;";
+app.get("/api/claims-calculator/selector_table", (req, res) => {
+  const sqlQuery = "SELECT * FROM `claims-calculator`.selector_table";
 
-//   db.query(sqlQuery, (err, results) => {
-//     if (err) {
-//       console.error("Error fetching data from database:", err);
-//       res.status(500).send("Error fetching data");
-//     } else {
-//       res.json(results);
-//     }
-//   });
-// });
+  db.query(sqlQuery, (err, results) => {
+    if (err) {
+      console.error("Error fetching data from database:", err);
+      res.status(500).send("Error fetching data");
+    } else {
+      res.json(results);
+    }
+    console.log(results);
+  });
+});
+
+// fetch(`http://localhost:3000/api/claims-calculator/selector_table`)
+//   .then((response) => response.json())
+//   .then((data) => console.log(data))
+//   .catch((error) => console.error("Error:", error));
